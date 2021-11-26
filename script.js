6f58fe7062b8d24c10b1bd807d7f39f085e32cf4
sharjeelyunus <sharjeel924@gmail.com>
2021-11-26 14:49:32 +0500

    📦 NEW: Starter

---
+++ script.js
@@ -0,0 +1,9 @@
+async function SearchProfile() {
+
+    let ProfileSec = document.getElementById('ProfileSec1');
+    let ProfileSec2 = document.getElementById('ProfileSec2');
+    ProfileSec.innerHTML = '';
+    ProfileSec2.innerHTML = '';
+
+    let Username = document.getElementById('SearchUsername');
+}
